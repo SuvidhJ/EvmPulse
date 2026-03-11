55f9cb36dc02260f3e09c07637ff1a2c79d22176
SuvidhJ <suvidh.jain2023@vitstudent.ac.in>
2026-03-11 11:47:53 +0530

    chore: project setup - TypeScript, Fastify, Jest, viem

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/test/**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  transform: { '^.+\\.ts$': 'ts-jest' },
+  testTimeout: 30000,
+};
